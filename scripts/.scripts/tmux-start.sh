f9e14fac3ca3d85e6e2179238bf937d4033c5796
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-11-04 14:38:25 -0700

    tweaks to comply with latest nvim

--- scripts/.scripts/tmux-start.sh
+++ scripts/.scripts/tmux-start.sh
@@ -2,12 +2,25 @@
 
 # Default session name
 DEFAULT_SESSION="SUMMONING DEMONS"
+EDITOR_WINDOW="altar"
+RUNNER_WINDOW="void"
+DOTFILES_WINDOW="arcana"
 
 # Check if tmux is already running
-if ! tmux has-session 2>/dev/null; then
-    # No sessions exist, so create a new one with the default name
-    tmux new-session -d -s "$DEFAULT_SESSION"
+if ! tmux has-session -t "$DEFAULT_SESSION" 2>/dev/null; then
+     
+    # Create windows
+    tmux new-session -d -s "$DEFAULT_SESSION" -n "$EDITOR_WINDOW"
+    tmux new-window -t "$DEFAULT_SESSION" -n "$RUNNER_WINDOW"
+    tmux new-window -t "$DEFAULT_SESSION" -n "$DOTFILES_WINDOW"
+
+    # Select the first window
+    tmux select-window -t "$DEFAULT_SESSION":"$EDITOR_WINDOW"
+    
+    # Send commands
+    tmux send-keys -t "$DEFAULT_SESSION":"$EDITOR_WINDOW" "demons" C-m
+    tmux send-keys -t "$DEFAULT_SESSION":"$DOTFILES_WINDOW" "cd ~/.dotfiles; clear" C-m
 fi
 
-# Attach to the existing session (whether it's the one we just created or a pre-existing one)
+# Attach to the existing session
 tmux attach-session
