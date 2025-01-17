7686250b82c34010215cddae64ac2da67a0103db
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-01-17 10:20:39 -0700

    removes resize subcommand from hyprland, tweaks to keymaps for zed, others

--- scripts/.scripts/tmux-start.sh
+++ scripts/.scripts/tmux-start.sh
@@ -5,6 +5,7 @@ DEFAULT_SESSION="SUMMONING DEMONS"
 EDITOR_WINDOW="altar"
 RUNNER_WINDOW="void"
 DOTFILES_WINDOW="arcana"
+LOGS_WINDOW="coven"
 
 # Check if tmux is already running
 if ! tmux has-session -t "$DEFAULT_SESSION" 2>/dev/null; then
@@ -13,6 +14,7 @@ if ! tmux has-session -t "$DEFAULT_SESSION" 2>/dev/null; then
     tmux new-session -d -s "$DEFAULT_SESSION" -n "$EDITOR_WINDOW"
     tmux new-window -t "$DEFAULT_SESSION" -n "$RUNNER_WINDOW"
     tmux new-window -t "$DEFAULT_SESSION" -n "$DOTFILES_WINDOW"
+    tmux new-window -t "$DEFAULT_SESSION" -n "$LOGS_WINDOW"
 
     # Select the first window
     tmux select-window -t "$DEFAULT_SESSION":"$EDITOR_WINDOW"
