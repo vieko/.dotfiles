a18d7b4e875b5fb68292dab107fb502d8deed1db
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-02-19 19:36:54 -0700

    tweaks highlights

--- nvim-redo/.config/nvim-redo/lua/config/highlights.lua
+++ nvim-redo/.config/nvim-redo/lua/config/highlights.lua
@@ -2,18 +2,21 @@ local M = {}
 
 M.setup = function()
   local hl = vim.api.nvim_set_hl
+  -- EDITOR
   hl(0, "CursorLine", { bg = "#2D323B" })
-  hl(0, "MiniCursorword", { bg = "#394B62" })
   hl(0, "Yank", { bg = "#394B62" })
+  hl(0, "Visual", { bg = "#446181" })
+  hl(0, "VirtColumn", { fg = "#20232A" }) -- #5B5D64 #20232A
+  hl(0, "MiniCursorword", { bg = "#394B62" })
+  -- GIT
+  hl(0, "GitSignsCurrentLineBlame", { fg = "#788CA6" })
+  -- SNACKS
   hl(0, "SnacksPicker", { bg = "#2F343E", fg = "#E6E6E6" })
   hl(0, "SnacksPickerTitle", { bg = "#2F343E", fg = "#ABB2BF" })
   hl(0, "SnacksPickerBorder", { bg = "#2F343E", fg = "#3C414C" })
-  hl(0, "SnacksPickerListCursorLine", { bg = "#363C46" })
   hl(0, "SnacksPickerPrompt", { fg = "#98C379" })
-  hl(0, "Visual", { bg = "#446181" })
-  hl(0, "ColorColumn", { bg = "none", fg = "none" })
-  hl(0, "VirtColumn", { fg = "#20232A" }) -- #5B5D64 #20232A
-  hl(0, "GitSignsCurrentLineBlame", { fg = "#788CA6" })
+  hl(0, "SnacksPickerListCursorLine", { bg = "#363C46" })
+  -- SYNTAX
 end
 
 return M.setup()
